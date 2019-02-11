5ed05425fe0201e5db86f5460110a045f033d175
Anny Shan <annyshan@annys-mbp.bc.edu>
2019-02-11 16:30:16 -0500

    5.5

--- To Do List/DetailViewController.swift
+++ To Do List/DetailViewController.swift
@@ -13,17 +13,23 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var toDoField: UITextField!
     var toDoItem: String?
     
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if let toDoItem = toDoItem {
             toDoField.text = toDoItem
         }
+        toDoField.becomeFirstResponder()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave" {
             toDoItem = toDoField.text
         }
+    }
+    
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
     }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
